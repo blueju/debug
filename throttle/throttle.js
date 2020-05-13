10fb2c29c29819013613dd39c6f633f1169cb42f
blueju <blueju@foxmail.com>
2020-05-13 08:05:10 +0800

    update throttleForSetTimeout

--- throttle/throttle.js
+++ throttle/throttle.js
@@ -5,6 +5,7 @@
  * 
  */
 
+//  时间戳版
 function throttleForTimestamp(fn, time) {
     let lasteTime = 0
     let newArgs = Array.prototype.slice.call(arguments, 2)
@@ -16,10 +17,29 @@ function throttleForTimestamp(fn, time) {
     }
 }
 
+//  定时器版
+function throttleForSetTimeout(fn, time) {
+    let timer = 0
+    let newArgs = Array.prototype.slice.call(arguments, 2)
+    return function (e) {
+        if (timer === 0) {
+            fn.call(fn, ...newArgs)
+            timer = false
+            return
+        }
+        if (timer) return
+        timer = setTimeout(() => {
+            fn.call(fn, ...newArgs)
+            clearTimeout(timer)
+        }, time);
+    }
+}
+
 function handle(p1, p2, p3) {
     console.log(Math.random());
     console.log(p1, p2, p3);
     console.log(this);
 }
 
-window.addEventListener('mousemove', throttleForTimestamp(handle, 2000, 1, 2, 3));
+// window.addEventListener('mousemove', throttleForTimestamp(handle, 2000, 1, 2, 3));
+window.addEventListener('mousemove', throttleForSetTimeout(handle, 2000, 1, 2, 3));
