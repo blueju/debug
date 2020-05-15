f25d86d9bef6c2b02753e9566abbdaa7418f2c85
blueju <blueju@foxmail.com>
2020-05-15 10:45:49 +0800

    update deepclone

--- DeepClone/v3.js
+++ DeepClone/v3.js
@@ -6,29 +6,17 @@
 function deepClone(target, map = new Map()) {
   if (typeof target === "object" && target !== null) {
     let result = Array.isArray(target) ? [] : {};
-    let map = new Map();
     if (map.get(target)) {
       return map.get(target);
     }
+    map.set(target, result);
     for (const key in target) {
       if (target.hasOwnProperty(key)) {
-        result[key] = deepClone(target[key]);
+        result[key] = deepClone(target[key], map);
       }
     }
-    map.set(target, result);
     return result;
   } else {
     return target;
   }
 }
-
-let one = {
-  a: undefined,
-  b: null,
-  c: 1,
-  d: [1, 2, 3],
-  e: { a: 1, b: 2, c: 3 },
-};
-
-let two = deepClone(one);
-console.log(two);
