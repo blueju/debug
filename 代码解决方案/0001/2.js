fbfa5921f9c8b68c0af3dc16a81f9901865a2bb3
blueju <blueju@foxmail.com>
2020-07-16 16:42:38 +0800

    update 解决方案0001

--- 代码解决方案/0001/2.js
+++ 代码解决方案/0001/2.js
@@ -1,11 +1,19 @@
 var str = "sfsf4dg3df5gdfg";
 
+// 方案一
 const newStr = Array.from(str).reduce((pre, cur, index) => {
   if (Number(cur) !== Number(cur)) {
+    // 英文字母
     return pre + cur;
   } else {
-    return pre;
+    // 数字
+    return pre + `[${cur}]`;
   }
 }, "");
-
 console.log(newStr);
+
+// 方案二
+const newStr2 = str.replace(/[1-9]/g, (replacement) => {
+  return `[${replacement}]`
+})
+console.log(newStr2);
--- 代码解决方案/0001/3.js
+++ 代码解决方案/0001/3.js
@@ -1,11 +1,19 @@
 var str = "sfsf4dg3df5gdfg";
 
+// 方案一
 const newStr = Array.from(str).reduce((pre, cur, index) => {
   if (Number(cur) !== Number(cur)) {
+    // 英文字母
     return pre + cur;
   } else {
+    // 数字
     return pre + (Number(cur) * 2).toString();
   }
 }, "");
-
 console.log(newStr);
+
+// 方案二
+const newStr2 = str.replace(/[1-9]/g, (replacement) => {
+  return `${(Number(replacement) * 2)}`
+})
+console.log(newStr2);
